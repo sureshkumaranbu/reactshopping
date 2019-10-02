dfe485c1e9b3fc0f9f699c5eed12a1d7a9f83e5e
Suresh Kumar Anbazhagan <sureshkumar.anbu88@gmail.com>
2019-10-01 21:09:01 -0400

    react shopping app

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_95vThbm9u28zATjCNZV9VCye00TPr1sa12';
+
+    const onToken = token => {
+        console.log(token);
+    }
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://picsum.photos/id/602/200/300'
+            description={`Your total is ${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
